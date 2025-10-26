9f057ece2d9f147971b85559c291139a0bcd90cb
D-Sketon <2055272094@qq.com>
2025-10-26 16:02:22 +0800

    Site updated: 2025-10-26 16:02:19

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1761413115616";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1761465731536";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
