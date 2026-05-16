6ab3bcc0135441724329ac51115306ac1091f223
D-Sketon <2055272094@qq.com>
2026-05-16 12:46:18 +0800

    Site updated: 2026-05-16 12:46:14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1777792729226";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1778906736671";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
